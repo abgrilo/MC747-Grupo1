ef52902d6e34ed67ae748527cc52687ac593c9a5
Alex Bredariol Grilo <abgrilo@gmail.com>
2011-04-17 20:34:05 -0300

    Criação do projeto rails para o portal

---
+++ portal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '3e0ecc7f42cf3f14a76dccb6e5f785bc123e47fca076db1cb08517963f5557391b2a11ff28420475eb6335506d1b1adcdf8d2546e3c4caaffc5af4456a6b15ba'
